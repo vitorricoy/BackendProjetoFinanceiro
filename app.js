5d7c8d7d05eba4251941f5f63f52c01a4c0f0788
Vitor Rodarte <vitorricoy@gmail.com>
2018-09-01 22:29:16 -0300

    Added cors

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 
 let port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/Transaction', transactionController);
 
 app.use('/api/Login', loginController);
