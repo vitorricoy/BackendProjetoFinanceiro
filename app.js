ae62ff736a774615f45814501c44b879060f8331
Vitor Rodarte <vitorricoy@gmail.com>
2018-12-22 22:53:46 -0200

    Corrected save transactions

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let port = process.env.PORT || 8080;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, grant-type");
   next();
 });
 
